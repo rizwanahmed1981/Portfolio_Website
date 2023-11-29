b8d0a25c388147f389b83a298d9d70b2a14b0b34
rizwanahmed1981 <109456571+rizwanahmed1981@users.noreply.github.com>
2023-11-29 13:21:24 +0500

    second

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,8 +9,8 @@ const Hero = (props) => (
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
-          Welcome To <br />
-          My Personal Portfolio
+         Hello Myself<br/>
+         "Rizwan Ahmed"
         </SectionTitle>
         <SectionText>
         "As a passionate and versatile web developer, I bring expertise in both frontend and backend technologies to craft seamless and dynamic digital experiences. With a keen eye for design and a robust foundation in coding, I thrive in professional environments where innovation meets functionality. Committed to excellence, I love translating clients' visions into interactive solutions. Let me turn your ideas into captivating websites that not only meet but exceed expectations. Eager to contribute to a collaborative and forward-thinking team while making a positive impact through my skills and dedication."
